d21703caf74b1a6ec83cd41f844ae6e0265ae6c2
Camren Davis <camrenad21@gmail.com>
2023-01-16 11:50:09 -0600

    Added Exception Handling

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Staks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Stack stacks = new Stack();
+            try 
+            {
+                stacks.Push("camren");
+                stacks.Push("mom");
+                stacks.Push("dad");
+                stacks.Push("sister");
+                stacks.Push("Cats");
+            }
+            catch(OverflowException e)
+            {
+                Console.WriteLine(e);
+            }
+
+            try 
+            {
+                Console.WriteLine("pop " + stacks.Pop());
+                Console.WriteLine("pop " + stacks.Pop());
+                Console.WriteLine("pop " + stacks.Pop());
+                Console.WriteLine("pop " + stacks.Pop());
+                Console.WriteLine("pop " + stacks.Pop());
+                Console.WriteLine("pop " + stacks.Pop());
+            }
+            catch(UnderFlowException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            stacks.Print();
+            stacks.Puts("hellos there");
+        }
+    }
+}
--- Stack.cs
+++ Stack.cs
@@ -8,78 +8,73 @@ using System.Linq;
 using System;
 using System.Collections.Generic;
 
-class Stack
-{
-    private string[] arr;
-    private int top;
-    public Stack()
+
+    public class UnderFlowException : Exception 
     {
-        arr = new string[5];
-        top = 0; 
-    }
-    public void Push(string name)
-    {
-        if (top >= 5)
+        public UnderFlowException() { }
+        public UnderFlowException(string message) 
+            : base(message) 
         {
-            Console.WriteLine("O V E R  F L O W ");
-            return;
         }
-        arr[top++] = name;  
+
     }
-    public void Print()
+
+namespace Staks
+{   
+    class Stack
     {
-        foreach(var name in arr)
+        private string[] arr;
+        private int top;
+        public Stack()
         {
-            Console.WriteLine(name);
+            arr = new string[5];
+            top = 0; 
         }
-    }
-    public string Pop()
-    {    
-        if (IsEmpty())
-        {   
-            Console.WriteLine("U N D E R  F L O W ");
-            return "E";
-        }
-        top--;
-        var data = arr[top];
-        arr = arr.Take(top).ToArray();
-        return data;    
-    }
-    public bool IsEmpty() 
-    {
-        if (top == 0) 
+        public void Push(string name)
         {
-            return true;
+            if (top >= 5)
+            {
+                throw new OverflowException("Stack is full");
+            }
+            arr[top++] = name;  
         }
-        return false;
-        
-    }
-    public void Puts(string text)
-    {
-        Console.WriteLine(text);
+        public void Print()
+        {
+            foreach(var name in arr)
+            {
+                Console.WriteLine(name);
+            }
+        }
+        public string Pop()
+        {    
+            if (IsEmpty())
+            {   
+                throw new UnderFlowException("underflow");
+            }
+            
+            top--;
+            var data = arr[top];
+            arr = arr.Take(top).ToArray();
+            return data;    
+        }
+        public bool IsEmpty() 
+        {
+            if (top == 0) 
+            {
+                return true;
+            }
+            return false;
+            
+        }
+        public void Puts(string text)
+        {
+            Console.WriteLine(text);
+        }
     }
 }
 
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Stack stacks = new Stack();
-        stacks.Push("camren");
-        stacks.Push("mom");
-        stacks.Push("dad");
-        stacks.Push("sister");
-        stacks.Push("Cats");
-        Console.WriteLine("pop " + stacks.Pop());
-        Console.WriteLine("pop " + stacks.Pop());
-        Console.WriteLine("pop " + stacks.Pop());
-        Console.WriteLine("pop " + stacks.Pop());
-        Console.WriteLine("pop " + stacks.Pop());
-        Console.WriteLine("pop " + stacks.Pop());
-        stacks.Print();
-        stacks.Puts("hellos there");
-    }
-}
+
+
 
 
